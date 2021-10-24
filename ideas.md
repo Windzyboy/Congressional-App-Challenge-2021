844ab105272f0334c228516abe5a22cee0e1b1b8
Windzyboy <63932310+Windzyboy@users.noreply.github.com>
2021-10-24 15:35:44 -0400

    Fonts and others

---
+++ ideas.md
@@ -0,0 +1,6 @@
+* App for easily creating GIFs with an FFMPEG backend
+* Social media platform
+* Bus tracker map
+* Weird mobile game?
+* 
+* Something with colors
--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+// Require dependencies for Electron
 const {app, BrowserWindow} = require('electron');
+const { url } = require('inspector');
 const path = require('path');
+
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        }
+        // webPreferences: {
+        //     preload: path.join(__dirname, 'preload.js'),
+        // }
+        icon: path.join(__dirname, 'directions_bus_yellow_24dp.svg')
     });
 
+    mainWindow.setTitle("School Bus Map")
     mainWindow.loadFile('index.html');
 }
 
+
+
+
 app.whenReady().then( () => {
     createWindow();
-
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow() });
 });
