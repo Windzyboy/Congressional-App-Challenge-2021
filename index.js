f02ed06ba82fd605497b890dc3040dc20b580c85
Windzyboy <63932310+Windzyboy@users.noreply.github.com>
2021-10-24 20:10:52 -0400

    move + update again

---
+++ index.js
@@ -0,0 +1,48 @@
+// Require dependencies for Electron
+const {app, BrowserWindow, globalShortcut} = require('electron');
+const { url } = require('inspector');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 400,
+        minHeight: 300,
+        // backgroundColor: '#ffff',
+        // frame: false,
+        titleBarStyle: 'hidden',
+        transparent: true,
+        // webPreferences: {
+        //     preload: path.join(__dirname, 'preload.js'),
+        // }
+        icon: path.join(__dirname, 'icon.ico')
+    });
+
+    mainWindow.setTitle("School Bus Map")
+    mainWindow.loadFile('src/index.html');
+}
+
+app.setUserTasks([
+    {
+      program: process.execPath,
+      arguments: '--new-window',
+      iconPath: process.execPath,
+      iconIndex: 0,
+      title: 'New Window',
+      description: 'Create a new window'
+    }
+]);
+
+app.whenReady().then( () => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow() });
+    globalShortcut.register('Control+Shift+I', () => {
+        return false;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
--- src/index.js
+++ src/index.js
@@ -1,48 +1,12 @@
-// Require dependencies for Electron
-const {app, BrowserWindow, globalShortcut} = require('electron');
-const { url } = require('inspector');
-const path = require('path');
+let map;
 
-function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        minWidth: 400,
-        minHeight: 300,
-        // backgroundColor: '#ffff',
-        // frame: false,
-        titleBarStyle: 'hidden',
-        transparent: true,
-        // webPreferences: {
-        //     preload: path.join(__dirname, 'preload.js'),
-        // }
-        icon: path.join(__dirname, 'icon.ico')
-    });
-
-    mainWindow.setTitle("School Bus Map")
-    mainWindow.loadFile('page/index.html');
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 44.344380778805565, lng: -73.10850732935332 },
+    zoom: 14,
+  });
 }
 
-app.setUserTasks([
-    {
-      program: process.execPath,
-      arguments: '--new-window',
-      iconPath: process.execPath,
-      iconIndex: 0,
-      title: 'New Window',
-      description: 'Create a new window'
-    }
-]);
-
-app.whenReady().then( () => {
-    createWindow();
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow() });
-    globalShortcut.register('Control+Shift+I', () => {
-        return false;
-    });
-});
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+function reloadMapIframe() {
+  document.getElementById('map').contentWindow.location.reload();
+}
